44980e8a59cf05298f9649ac9a247e693c7bd966
Mohammad Farukh <65119875+iamFarukh@users.noreply.github.com>
2022-09-05 18:50:43 +0530

    Create 86 (25 August 2022) countEleLessThanOrEqual.cpp
    countEleLessThanOrEqual

---
+++ 86 (25 August 2022) countEleLessThanOrEqual.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// C++ implementation of For each element in 1st 
+// array count elements less than or equal to it
+// in 2nd array
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+
+// } Driver Code Ends
+// function to count for each element in 1st array,
+// elements less than or equal to it in 2nd array
+class Solution{
+  public:
+    vector<int> countEleLessThanOrEqual(int arr1[], int arr2[],int m, int n) {
+        
+        sort(arr2, arr2+n);
+        vector<int> result(m);
+        for(int i = 0; i<m; i++) {
+            auto it = upper_bound(arr2, arr2+n, arr1[i]) - arr2;
+            result[i] = it;
+        }
+        
+        return result;
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m,n;
+        cin>>m>>n;
+        int arr1[m],arr2[n];
+        for(int i=0;i<m;i++)
+        cin>>arr1[i];
+        for(int j=0;j<n;j++)
+        cin>>arr2[j];
+        Solution obj;
+        vector <int> res = obj.countEleLessThanOrEqual(arr1, arr2, m, n);
+        for (int i = 0; i < res.size (); i++) cout << res[i] << " ";
+        
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
