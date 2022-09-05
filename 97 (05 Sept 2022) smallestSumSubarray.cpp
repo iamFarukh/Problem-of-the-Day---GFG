938f31abc9740370f4f12bb3d78f93d7f5033897
Mohammad Farukh <65119875+iamFarukh@users.noreply.github.com>
2022-09-05 19:00:45 +0530

    Create 97 (05 Sept 2022) smallestSumSubarray.cpp
    smallestSumSubarray

---
+++ 97 (05 Sept 2022) smallestSumSubarray.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+  int smallestSumSubarray(vector<int>& a){
+      //Code here
+      int cs = INT_MAX, ms = 0;
+     for(int i=0; i<a.size(); i++){
+         ms += a[i];
+         cs = min(cs, ms);
+         if(ms>0) ms=0;
+         
+   
+     }
+     return cs;
+  }
+};
+
+
+//{ Driver Code Starts.
+
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> a(n);
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        
+        Solution ob;
+        
+        int ans = ob.smallestSumSubarray(a);
+        
+        cout<<ans<<endl;
+    }
+}
+// } Driver Code Ends
