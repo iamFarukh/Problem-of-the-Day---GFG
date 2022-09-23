0d545c29dd4087422ff5e70f0cc2dfece0bcf74a
Mohammad Farukh <65119875+iamFarukh@users.noreply.github.com>
2022-09-23 13:20:04 +0530

    Create 111 ( 19 Sept 2022) leftSmaller.cpp
    leftSmaller

---
+++ 01 September Problems/111 ( 19 Sept 2022) leftSmaller.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<int> leftSmaller(int n, int a[]){
+        // code here
+        vector<int>ans(n);
+        stack<int>st;
+        st.push(-1);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && st.top()>=a[i]){
+                st.pop();
+            }
+            ans[i]=st.top();
+            st.push(a[i]);
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
+        Solution ob;
+        vector<int> ans = ob.leftSmaller(n, a);
+        for(int i = 0;i < n;i++)
+            cout<<ans[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
